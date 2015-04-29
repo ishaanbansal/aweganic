585bddc7800fb8c06bc296bd95bd8563a87bc350
ishaanbansal <ishaan.bansal.29@gmail.com>
2015-04-29 23:49:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://ninja:ninja69@ds031611.mongolab.com:31611/ninjadb');
+mongoose.connect('mongodb://localhost/Test');
 
 module.exports = mongoose.connection;
